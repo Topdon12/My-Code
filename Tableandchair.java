137abaa79d6f934936f7ea7b7306e5bbc4963a9d
Ahorlu Dennis-04-2024-2109D <ahorludennis12@gmail.com>
2026-06-11 10:06:36 +0000

    Add files via upload

---
+++ Tableandchair.java
@@ -0,0 +1,19 @@
+public class Tableandchair {
+    public static void main(String[] args){
+
+        System.out.println("X                                                       X");
+        System.out.println("X                                                       X");
+        System.out.println("X                                                       X");
+        System.out.println("X                                                       X");
+        System.out.println("X                                                       X");
+        System.out.println("XXXXXXXXXXXX        XXXXXXXXXXXXXXX           XXXXXXXXXXX");
+        System.out.println("X          X        X             X           X         X");
+        System.out.println("X          X        X             X           X         X");
+        System.out.println("X          X        X             X           X         X");
+        System.out.println("X          X        X             X           X         X");
+        System.out.println("X          X        X             X           X         X");
+
+    }
+
+
+}
