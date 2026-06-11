26643cf86f7300b94fc7b41fb4af9dbef7dfb227
Ahorlu Dennis-04-2024-2109D <ahorludennis12@gmail.com>
2026-06-11 10:06:12 +0000

    Add files via upload

---
+++ Mountain.java
@@ -0,0 +1,15 @@
+public class Mountain {
+    public static void main (String[] args){
+
+
+        System.out.println("                       TT                       ");
+        System.out.println("                      TTTT                      ");
+        System.out.println("                    TTTTTTTT                    ");
+        System.out.println("                TTTTTTTTTTTTTTTT                ");
+        System.out.println("            TTTTTTTTTTTTTTTTTTTTTTTT            ");
+        System.out.println("        TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT        ");
+        System.out.println("    TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT    ");
+        System.out.println("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
+    }
+
+}
